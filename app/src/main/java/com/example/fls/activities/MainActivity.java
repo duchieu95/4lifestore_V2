4a1c6456afc30e31be58d3434967d8e0c67d85e1
HieuHo <hoduchieu.qn95@gmail.com>
2017-03-04 10:58:36 +0700

    update file

--- app/src/main/java/com/example/fls/activities/MainActivity.java
+++ app/src/main/java/com/example/fls/activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -99,5 +100,11 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
